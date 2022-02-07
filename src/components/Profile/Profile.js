b1ca288330a2b0821e2b3c741429d82a56c9d9ad
Alexandr Sorokin <sanek1678@gmail.com>
2022-02-07 21:13:43 +0200

    First task

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,31 +1,42 @@
-function Profile(props) {
-    return (<div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
+import PropTypes from 'prop-types';
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>)
+function Profile({ avatar, name, tag, location, stats }) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers:</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views:</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes:</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
 export default Profile;
